58b033cb801e05f481468eb8ebb72c29f2cb2a72
Javier Calatravallaveria <javier.calatravallaveria@altran.com>
2017-08-22 08:11:57 +0200

    Base XCode project

---
+++ ArchTemp/ArchTemp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArchTemp
+//
+//  Created by 08APO0516 on 22/08/2017.
+//  Copyright © 2017 08APO0516ja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
